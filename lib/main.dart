913f76066ad202401a3837c537430fb943f02ef0
MarinBond <107083262+VictorNaz@users.noreply.github.com>
2022-08-08 19:37:23 -0300

    V1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+import 'dart:html';
 
+import 'package:flutter/cupertino.dart';
+
+void main(List<String> args) {
+  runApp(const ProjectSeth());
+}
+
+class ProjectSeth extends StatelessWidget {
+  const ProjectSeth({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
