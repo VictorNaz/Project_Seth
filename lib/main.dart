44f4c120b3ba37e731c1561aa8e5fbb6b46e3543
Pedro Vinicius Veiga <99768449+PedroViniciusVeiga@users.noreply.github.com>
2022-08-08 21:47:21 -0300

    V1.5
    Organização pastas

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,3 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(const ProjectSeth());
 }
-
-class ProjectSeth extends StatelessWidget {
-  const ProjectSeth({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomePage());
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/image/image_login.png'),
-                fit: BoxFit.fill)),
-      ),
-    );
-  }
-}
