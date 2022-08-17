1dc5b18da08470b7211edb0d08850371c2d757f9
MarinBond <107083262+VictorNaz@users.noreply.github.com>
2022-08-17 20:10:55 -0300

    V5.0

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'loginpage.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,21 +12,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(
+        SystemUiMode.immersiveSticky); //código para desativar a barra de tarefa
+
     return Container(
+      //imagem de backgroud.
       decoration: const BoxDecoration(
-        image: DecorationImage(image: AssetImage('assets/image/fundo.png')),
+        image: DecorationImage(
+            image: AssetImage('assets/image/fundo.png'), fit: BoxFit.cover),
       ),
+      //imagem da logo SETH.
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Padding(
-            padding: EdgeInsets.only(top: 250),
+            padding: EdgeInsets.only(top: 170, bottom: 100),
             child: Image(
-              image: AssetImage('assets/image/logomarca.png'),
+              image: AssetImage('assets/image/logomarca2.png'),
+              height: 200,
             ),
           ),
+          //Paddin para mover as imagens na tela.
           Padding(
-            padding: const EdgeInsets.only(top: 300),
+            padding: const EdgeInsets.only(top: 200),
+            //botão de login.
             child: TextButton(
                 onPressed: () {
                   Navigator.push(
@@ -33,16 +43,18 @@ class _HomePageState extends State<HomePage> {
                     MaterialPageRoute(builder: (context) => const LoginPage()),
                   );
                 },
+                //configurações do button
                 style: TextButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 252, 72, 27),
+                  backgroundColor: const Color.fromARGB(255, 252, 72, 27),
                   fixedSize: const Size(350, 50),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
+                //texto do button e estilo da escrita
                 child: const Text(
-                  "Login",
-                  style: TextStyle(color: Colors.white, fontSize: 27),
+                  "Entrar",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
                 )),
           )
         ],
