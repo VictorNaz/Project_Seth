4379e8514c6c5790272f6a61c90db7ac3fd9241d
MarinBond <107083262+VictorNaz@users.noreply.github.com>
2022-08-22 19:10:03 -0300

    Revert "V7.4"
    This reverts commit 13b89478e77145cae621502ad81f34e628adf10d.

--- lib/cadAluno.dart
+++ lib/cadAluno.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unnecessary_const
-
 import 'package:flutter/material.dart';
 
 class CadAluno extends StatefulWidget {
@@ -20,80 +18,7 @@ class _CadAlunoState extends State<CadAluno> {
         body: Center(
           child: Column(
             children: [
-              const Padding(padding: EdgeInsets.only(top: 40)),
-              const SizedBox(
-                width: 325,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Nome",
-                    hintStyle: TextStyle(color: Colors.black),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromARGB(255, 252, 72, 27))),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 325,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Email",
-                    hintStyle: TextStyle(color: Colors.black),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromARGB(255, 252, 72, 27))),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 325,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "CPF",
-                    hintStyle: TextStyle(color: Colors.black),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromARGB(255, 252, 72, 27))),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 325,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Senha",
-                    hintStyle: TextStyle(color: Colors.black),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromARGB(255, 252, 72, 27))),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 325,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Confirmar Senha",
-                    hintStyle: TextStyle(color: Colors.black),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromARGB(255, 252, 72, 27))),
-                  ),
-                ),
-              ),
-              TextButton(
-                  onPressed: null,
-                  style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 252, 72, 27),
-                    fixedSize: const Size(330, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ), //texto do button e estilo da escrita
-                  child: const Text(
-                    "Cadastrar-se",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  )),
+              const Padding(padding: EdgeInsets.only(top: 100)),
             ],
           ),
         ));
