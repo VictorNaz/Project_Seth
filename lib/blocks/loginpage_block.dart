9b76a07b6235f26ba9b84e1ef6eded8ecab04753
Pedro Vinicius Veiga <99768449+PedroViniciusVeiga@users.noreply.github.com>
2022-08-24 21:44:49 -0300

    V9.3

---
+++ lib/blocks/loginpage_block.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter_project_seth/validators/loginValidators.dart';
+import 'package:rxdart/rxdart.dart';
+
+class LoginBlock extends BlocBase with LoginValidators{
+  final emailController = BehaviorSubject<String>();
+  final passwordController = BehaviorSubject<String>();
+
+//Recebe os dados e retorna o valor correto, já validado conforme o objeto validate...
+  Stream<String> get outEmail => emailController.stream.transform(validateEmail);
+  Stream<String> get outPassword => passwordController.stream.transform(validatePassword);
+
+
+
+  @override
+  void dispose() {
+    emailController.close();
+    passwordController.close();
+  }
+}
