658f4fb854e79ced839385157b749590f7dca231
MarinBond <107083262+VictorNaz@users.noreply.github.com>
2022-08-29 18:40:05 -0300

    V11

--- lib/cadAluno.dart
+++ lib/cadAluno.dart
@@ -16,7 +16,7 @@ class _CadAlunoState extends State<CadAluno> {
         FocusScopeNode currentFocus = FocusScope.of(context);
 
         if (!currentFocus.hasPrimaryFocus) {
-          //Foco primario
+          //Foco no campo primario
           //Desfocar
           currentFocus.unfocus();
         }
--- lib/menuPrincipal.dart
+++ lib/menuPrincipal.dart
@@ -18,6 +18,94 @@ class _MenuState extends State<Menu> {
         backgroundColor: const Color.fromARGB(255, 252, 72, 27),
         actions: const [],
       ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(),
+          //Posicionamento do campo ao selecionar o campo
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //Botão autoavaliação
+                    TextButton(
+                        onPressed: null,
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 252, 72, 27),
+                          fixedSize: const Size(330, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ), //texto do button e estilo da escrita
+                        child: const Text(
+                          "Autoavaliação",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                    TextButton(
+                        onPressed: null,
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 252, 72, 27),
+                          fixedSize: const Size(330, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ), //texto do button e estilo da escrita
+                        child: const Text(
+                          "Registrar Desempenho",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                        onPressed: null,
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 252, 72, 27),
+                          fixedSize: const Size(330, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ), //texto do button e estilo da escrita
+                        child: const Text(
+                          "Quadro de Aulas",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                    TextButton(
+                        onPressed: null,
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 252, 72, 27),
+                          fixedSize: const Size(330, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ), //texto do button e estilo da escrita
+                        child: const Text(
+                          "Informações",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+        ],
+      ),
     );
   }
 }
