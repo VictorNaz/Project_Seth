54c388ba712ef32920f3a447d8bfb252b0e03d04
Pedro Vinicius Veiga <99768449+PedroViniciusVeiga@users.noreply.github.com>
2022-09-19 19:06:01 -0300

    V20.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
 import 'frontEnd/geral/Homepage.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  final server = await shelf_io.serve(
+      (request) => Response(200, body: "ok"), 'localhost', 8080);
+  print("object server = http://localhost:8080");
   runApp(const ProjectSeth());
 }
 
