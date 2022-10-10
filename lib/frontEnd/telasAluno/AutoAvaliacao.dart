591d9601dac1e7b6b60924777fa6ebdf0a363112
MarinBond <107083262+VictorNaz@users.noreply.github.com>
2022-10-10 19:49:17 -0300

    V31.3

--- lib/frontEnd/telasAluno/AutoAvaliacao.dart
+++ lib/frontEnd/telasAluno/AutoAvaliacao.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_seth/frontEnd/telasAluno/menuPrincipal.dart';
+import 'package:kg_charts/kg_charts.dart';
+import 'package:kg_charts/src/flutter_radar_map.dart';
 
 import '../widgets/utilClass.dart';
 
@@ -37,8 +39,55 @@ class _AutoAvaliacaoState extends State<AutoAvaliacao> {
           texto: "Opções",
         ),
       ),
-      //botão flutuante sobre a barra inferior
 
+      body: Center(
+        child: Column(
+          children: [
+            RadarWidget(
+              radarMap: RadarMapModel(
+                legend: [
+                  LegendModel('Desempenho 10/10', const Color(0XFF0EBD8D)),
+                ],
+                indicator: [
+                  IndicatorModel("Alimentação", 10),
+                  IndicatorModel("Prevenção", 10),
+                  IndicatorModel("Atividade Fisica", 10),
+                  IndicatorModel("Comportamento", 10),
+                  IndicatorModel("Relacionamento", 10),
+                  IndicatorModel("Espiritual", 10),
+                  IndicatorModel("Defesa Pessoal", 10),
+                ],
+                data: [
+                  MapDataModel([10, 7, 8, 10, 6, 9, 10]),
+                ],
+                radius: 100,
+                duration: 2000,
+                shape: Shape.square,
+                maxWidth: 70,
+                line: LineModel(10),
+              ),
+              textStyle: const TextStyle(color: Colors.black, fontSize: 14),
+              isNeedDrawLegend: true,
+              lineText: (p, length) => "${(p + 1 ~/ length)}",
+              dilogText: (IndicatorModel indicatorModel,
+                  List<LegendModel> legendModels, List<double> mapDataModels) {
+                StringBuffer text = StringBuffer("");
+                for (int i = 0; i < mapDataModels.length; i++) {
+                  text.write(
+                      "${legendModels[i].name} : ${mapDataModels[i].toString()}");
+                  if (i != mapDataModels.length - 1) {
+                    text.write("\n");
+                  }
+                }
+                return text.toString();
+              },
+              outLineText: (data, max) => "${data = data}",
+            ),
+          ],
+        ),
+      ),
+
+      //botão flutuante sobre a barra inferior
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
