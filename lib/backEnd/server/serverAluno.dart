7d2f45d9c544d1aa5be71fb372e0982b19ccdb5e
Pedro Vinicius Veiga <99768449+PedroViniciusVeiga@users.noreply.github.com>
2022-10-10 19:12:39 -0300

    V31.1

--- lib/backEnd/server/serverAluno.dart
+++ lib/backEnd/server/serverAluno.dart
@@ -6,9 +6,14 @@ import 'package:http/http.dart' as http;
 //Pendencias
 //Validação de campos para dados repetidos
 //Criptografia de senha
+
+var headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Basic YWRtaW46QWxlc2V0aCFAIw=='
+};
+
 class ServerAluno {
   static Future<void> cadastrarAluno(Aluno aluno) async {
-    var headers = {'Content-Type': 'application/json'};
     var request = http.Request(
         'POST', Uri.parse('https://apiseth.cyclic.app/cadastrarAluno'));
     request.body = json.encode({
@@ -32,7 +37,6 @@ class ServerAluno {
 
   static Future<void> valPresenAluno(Aluno aluno) async {
     try {
-      var headers = {'Content-Type': 'application/json'};
       String url_Api = 'https://apiseth.cyclic.app/validaPresenca';
       var request = http.Request('POST', Uri.parse(url_Api));
       request.body = json.encode({
@@ -56,7 +60,6 @@ class ServerAluno {
   }
 
   static Future<String> buscaAlunoId(Aluno aluno) async {
-    var headers = {'Content-Type': 'application/json'};
     var request = http.Request(
         'POST', Uri.parse('https://apiseth.cyclic.app/buscaAlunoId'));
     request.body = await json.encode({"usuario": aluno.usuario});
@@ -75,7 +78,6 @@ class ServerAluno {
   }
 
   static Future<void> iniciaProgresso(Aluno aluno) async {
-    var headers = {'Content-Type': 'application/json'};
     var request = http.Request(
         'POST', Uri.parse('https://apiseth.cyclic.app/iniciaProgresso'));
     request.body = json.encode({"id": aluno.id});
@@ -92,7 +94,6 @@ class ServerAluno {
   }
 
   static Future<bool> verificaUsuario(Aluno aluno) async {
-    var headers = {'Content-Type': 'application/json'};
     var request = http.Request(
         'POST', Uri.parse('https://apiseth.cyclic.app/buscaUsuarioAluno'));
     request.body = json.encode({"usuario": aluno.usuario});
@@ -114,7 +115,6 @@ class ServerAluno {
   }
 
   static Future<String> logaUsuario(Aluno aluno) async {
-    var headers = {'Content-Type': 'application/json'};
     var request = http.Request(
         'POST', Uri.parse('https://apiseth.cyclic.app/loginUsuario'));
     request.body =
