643f6c58e5389f21b7a471f920862544f30a52e3
VictorNaz <victormaier@hotmail.com>
2023-08-09 21:07:56 -0300

    V104.1

--- lib/frontEnd/widgets/utilClass.dart
+++ lib/frontEnd/widgets/utilClass.dart
@@ -5,7 +5,6 @@ import 'dart:io';
 import 'dart:ui';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_project_seth/backEnd/controladora/CtrlAluno.dart';
 import 'package:flutter_project_seth/backEnd/modelo/faixa.dart';
 import 'package:flutter_project_seth/backEnd/security/sessionService.dart';
 import 'package:flutter_project_seth/frontEnd/geral/Homepage.dart';
